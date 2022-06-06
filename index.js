2a5492caa5558f39e5d3f93b8855b8ba571d0287
Douglas Obara <douglasobara97@gmail.com>
2022-06-06 17:26:51 +0300

    commit tree 8d0188bc2d1434553f87a8e971af3d81e11f52f1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// insert to end of an array
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// insert to beginning of an array
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// removes last item in an array
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// removes first item in an array
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+/* appends a cat to the cats array and returns a new array, leaving the cats array unchanged */
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+/* prepends a cat to the cats array and returns a new array, leaving the cats array unchanged: */
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+/* removes the last cat in the cats array and returns a new array, leaving the cats array unchange */
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+/* removes the first cat from the cats array and returns a new array, leaving the cats array unchanged */
+function removeFirstCat() {
+  return cats.slice(1);
+}
